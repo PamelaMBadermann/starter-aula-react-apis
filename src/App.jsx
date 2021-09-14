2c1998316937be90363486a845d522aa9c856a10
Pamela Badermann <badermann.pamela@gmail.com>
2021-09-14 20:26:03 -0300

    hellou world

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import CssBaseline from '@material-ui/core/CssBaseline'
+
+import Routes from './config/Routes';
+
+export default function App() {
+    return (
+        <>
+            <CssBaseline/>
+            <Routes/>
+        </>
+    )
+}
---
+++ src/config/Routes.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home from '../pages/Home';
+
+export default function Routes() {
+    return(
+        <BrowserRouter>
+            <Switch>
+                <Route to='/' exact component={Home}/>
+            </Switch>
+        </BrowserRouter>
+    )
+}
